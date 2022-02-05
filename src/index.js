77525e9f776c0d35140c2f054b9e8b5e8ebb0c9d
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-02-04 18:49:09 -0600

    redeploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Translate from "./Translate";
-
 ReactDOM.render(
   <React.StrictMode>
     <Translate />
